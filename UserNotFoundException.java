4e9fafac08a23c35f22ba131da66a9590cc452ce
manish-dev2 <67372008+manish-dev2@users.noreply.github.com>
2020-06-26 16:21:36 +0530

    Update UserNotFoundException.java

--- UserNotFoundException.java
+++ UserNotFoundException.java
@@ -1,5 +1,3 @@
-package com.javatpoint.server.main.user;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
